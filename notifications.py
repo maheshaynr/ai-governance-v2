0e5cbb15453fa9a85633333a178145b1d4a4ccc5
Mahesh <maheshaynr@github.com>
2026-07-05 12:38:01 +0530

    chore: change default SMTP host from office365 to gmail

--- notifications.py
+++ notifications.py
@@ -12,7 +12,7 @@ class EmailNotifier:
         if not to_email or "@" not in to_email:
             return
 
-        smtp_host = os.environ.get('SMTP_HOST', 'smtp.office365.com')
+        smtp_host = os.environ.get('SMTP_HOST', 'smtp.gmail.com')
         smtp_port = int(os.environ.get('SMTP_PORT', 587))
         smtp_user = os.environ.get('SMTP_USER')
         smtp_pass = os.environ.get('SMTP_PASSWORD')
